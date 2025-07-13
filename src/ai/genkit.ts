299c03644332f7a5dfa0e438c6d00bdfac1603ed
Vishal-WD <vishalchandran6126@gmail.com>
2025-07-13 21:59:48 +0530

    Credentials modified

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-const GEMINI_API_KEY = 'AIzaSyADrmwyKmf2o5vB5UXy_ocnM4BFKyfmyCQ';
+const GEMINI_API_KEY = 'AIzaSyBSizLbiKd8kacHov54K-Ao-bbObVcDn3s';
 
 // Configure googleAI plugin with the API key
 const googleAIPlugin = googleAI({
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,10 +5,11 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADrmwyKmf2o5vB5UXy_ocnM4BFKyfmyCQ",
+  apiKey: "AIzaSyAOP9Q04fOzNOrzT1Tu24uB96m1zvbtdzc",
   authDomain: "csp21-a0023.firebaseapp.com",
+  databaseURL: "https://csp21-a0023-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "csp21-a0023",
-  storageBucket: "csp21-a0023.appspot.com",
+  storageBucket: "csp21-a0023.firebasestorage.app",
   messagingSenderId: "228527630242",
   appId: "1:228527630242:web:1811bf5b68c1d2ea4874fb",
   measurementId: "G-R38L26WKVP"
